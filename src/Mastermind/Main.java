650ff5547a070d4502bc9e4127c2e2ebb9858925
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-09 22:57:21 +0200

    main structure

--- src/Mastermind/Main.java
+++ src/Mastermind/Main.java
@@ -1,5 +1,34 @@
 package Mastermind;
 
 public class Main {
+	public static void main(String[] args) throws Exception
+	  {
+	    while (true)
+	    {
+	      setDifficulty();
+	      shuffle();
+	      game();
+	      if (!replay()) break;
+	    }
+	  }
 
+	private static boolean replay() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static void game() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void shuffle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void setDifficulty() {
+		// TODO Auto-generated method stub
+		
+	}
 }
