a559521995aa86b2fe14b230f772cab8403c8ebb
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-10 20:56:03 +0200

    game works

--- src/Mastermind/Mastermind.java
+++ src/Mastermind/Mastermind.java
@@ -9,33 +9,109 @@ public class Mastermind {
 	static String colourCode = "rbygop";  
 
 	static String secretCode = "abcd"; 
+	// mastermind convention :
+	//  black : correct animal at correct position
+	//  white : correct animal at incorrect position
+	static int blackScore, whiteScore; 
 
 
+	static String getPlayerGuess() {
+	  boolean inputDone=false;
+	  String line="";
+	  while (!inputDone) {
+	    System.out.println("Colour Code : "+ colourCode);
+	    System.out.print("Enter your guess : ");
+	    line = getInputLine();
+	    String error = validateInput(line);
+	    if (error.equals("OK"))
+	      inputDone=true;
+	    else
+	      System.out.println(error);
+	  }
+	  return line.toLowerCase();
+	}
+ 
+	static String getInputLine() {
+	  try {
+	    String line = new java.io.BufferedReader(
+	      new java.io.InputStreamReader(System.in)
+	    ).readLine();
+	 
+	    return line;
+	  } 
+	  catch (Exception e) {
+	    return "";
+	  }
+	} 
+	
+	static String validateInput(String line) {
+	  line = line.toLowerCase();
+	  if (line.length()!=4) return "Must enter 4 letters";
+	 
+	  // check each character
+	  boolean[] choice=new boolean[colourNames.length];
+	  for (int i=0;i<line.length();i++)
+	  {
+	    char ch=line.charAt(i);
+	    int j=getColourIndex(ch);
+	    if (j==-1) return "Invalid Colour";
+//	    if (choice[j]) return "Repeated Colour.";
+	    choice[j]=true;
+	  }
+	   
+	  return "OK";
+	}
+	
+	static int getColourIndex(char a) {
+	  return colourCode.indexOf(a);
+	}
 
-  static String getPlayerGuess() {
-    return "ABCD";  
-  }
+	
+
+
+	static boolean calculateScore(String guess)	{
+		blackScore = whiteScore = 0;
+		for (int i = 0; i < guess.length(); i++) {
+			char ch = guess.charAt(i);
+			int j = secretCode.indexOf(ch);
+			if (i == j) blackScore++;
+			if (j >= 0) whiteScore++;
+		}
+		boolean success=false;
+		if ( blackScore==4) success=true;
+//		if (difficulty==EASY && whiteScore==4) success=true;
+		return success;
+	}
+
+	static void displayScore(String guess) {
+		String s = "";
+
+		s = "Guess: [";
+		for (int i = 0; i < guess.length(); i++){
+			if (i!=0) s += ", ";
+			int j = colourCode.indexOf(guess.charAt(i));
+			s += colourNames[j];
+		}
+		s += "]";
+
+		s += " injured is " + whiteScore + "   dead is " + blackScore;
+
+
+		System.out.println(s);
+	}
+
+	static void displaySolution() {
+		String s = "[";
+		for (int i = 0; i < 4; i++) {
+			if (i != 0) s += ", ";
+			s += colourNames[ permutation[i] ];
+		}
+		s += "]";
+
+		System.out.println(s);
+	}
+
  
-  static boolean calculateScore(String guess) {
-    return true;
-  }
- 
-  static void displayScore(String guess) {
-  }
- 
-  static void displaySolution() {
-    String s = "[";
-    for (int i = 0; i < 4; i++) {
-      if (i != 0) s += ", ";
-      s += colourNames[ permutation[i] ];
-    }
-    s += "]";
-   
-    System.out.println(s);
-  }
- 
-  static void setDifficulty() {
-  }
  
   //  Knuth Shuffling algorithm to randomize the permutation array
   static void shuffle() {
@@ -69,7 +145,6 @@ public class Mastermind {
  
   public static void main(String[] args) throws Exception {
     while (true) {
-      setDifficulty();
       shuffle();
       game();
       if (!replay()) break;
